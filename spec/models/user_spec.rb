f5f66bad779e3ed38a6c79477c6a98a5111cc8cb
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-01 19:02:02 +0900

    ユーザー新規登録機能の新規登録できるときのexample列挙

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,18 @@ RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
   end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できるとき' do
+      it '全ての項目が入力されていれば登録できる' do
+      end
+      it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
+      end
+      it 'family_nameとfirst_nameが全角だと登録できる' do
+      end
+      it 'family_name_huriganaとfirst_name_huriganaが全角カタカナだと登録できる' do
+      end
+    end
+    
+  end
 end
