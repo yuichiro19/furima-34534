ef69d2be13f171fec9a0a4552da4fac473321886
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-01 23:10:10 +0900

    新規登録できるときのテストコード記入

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,12 +8,29 @@ RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
     context '新規登録できるとき' do
       it '全ての項目が入力されていれば登録できる' do
+        expect(@user).to be_valid
       end
       it 'passwordとpassword_confirmationが6文字以上であれば登録できる' do
+        @user.password = '000000'
+        @user.password_confirmation = '000000'
+        expect(@user).to be_valid
       end
-      it 'family_nameとfirst_nameが全角だと登録できる' do
+      it 'family_nameが全角だと登録できる' do
+        @user.family_name = 'あア亜'
+        expect(@user).to be_valid
       end
-      it 'family_name_huriganaとfirst_name_huriganaが全角カタカナだと登録できる' do
+      it 'first_nameが全角だと登録できる' do
+        @user.first_name = 'あア亜'
+        expect(@user).to be_valid
+      end
+
+      it 'family_name_huriganaが全角カタカナだと登録できる' do
+        @user.family_name_hurigana = 'アアア'
+        expect(@user).to be_valid
+      end
+      it 'first_name_huriganaが全角カタカナだと登録できる' do
+        @user.first_name_hurigana = 'アアア'
+        expect(@user).to be_valid
       end
     end
     
