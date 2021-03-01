cbcbd2a0618b32895fab7e61754ca478541ed0ee
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-01 19:08:39 +0900

    userモデル、バリデーションの訂正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_name_hurigana, presence: true
-  validates :first_name_hurigana, presence:true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+    validates :family_name
+    validates :first_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/  } do
+    validates :family_name_hurigana
+    validates :first_name_hurigana
+  end
   validates :birthday, presence: true
   
 end
