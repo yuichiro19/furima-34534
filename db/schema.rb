50be82567c9f9659af6dd895b9d85f5679a0b8f0
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-02 18:08:39 +0900

    itemモデル作成、マイグレーション実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_28_073028) do
+ActiveRecord::Schema.define(version: 2021_03_02_085801) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "item_name"
+    t.text "text"
+    t.integer "price"
+    t.integer "category_id"
+    t.integer "status_id"
+    t.integer "shipping_fee_id"
+    t.integer "prefucture_id"
+    t.integer "date_of_shipment_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
