f4238fb738f50869ba464fa95cefaf6d2fa3fc74
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-02 17:06:08 +0900

    passwordのFaker修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name                  { 'test' }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { '1a' + Faker::Internet.password(min_length: 4) }
     password_confirmation { password }
     family_name           { '確認' }
     first_name            { '太郎' }
