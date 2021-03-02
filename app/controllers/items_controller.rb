00c121bad5dbecc57f7c15259d31b15747e509f4
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-02 19:08:30 +0900

    ストロングパラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,15 @@
 class ItemsController < ApplicationController
   def index
   end
+
+
+
+
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :image, :text, :price, :category_id, :status_id, :shipping_fee_id, :prefucture_id, :date_of_shipment_id).merge(user_id: current_user.id)
+  end
 end
