108a8c4b6c4209b7eabec248504b387dfcbe0ea6
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-04 10:59:53 +0900

    itemモデルのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,5 +8,20 @@ class Item < ApplicationRecord
     has_one_attached :image
     belongs_to :user
 
-    
+    with_options presence: true do
+        validates :item_name
+        validates :text
+        validates :image
+    end
+
+    with_options numericality: { other_than: 0, message:'Select' } do
+        validates :category_id
+        validates :status_id
+        validates :shipping_fee_id
+        validates :prefucture_id
+        validates :date_of_shipment_id
+    end
+
+    validates :price, presence :true, format: { with: /\A[0-9]+\z/, message: 'Half-width number'}, 
+    numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'Out of setting range' }
 end
