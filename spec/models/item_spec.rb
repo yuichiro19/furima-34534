992eb97b523d29d7704a6ff2cf9e30c4e5f772a0
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-05 16:23:47 +0900

    テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -75,7 +75,7 @@ RSpec.describe Item, type: :model do
       it 'userが紐づいていなければ出品できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('User must exist')
+        expect(@item.errors.full_messages).to include "User must exist"
       end
       
       
