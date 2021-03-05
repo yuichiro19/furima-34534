4dfcd961b914e6910d491556aeb001895b86f887
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-05 10:41:53 +0900

    itemモデル単体テスト用のFactoryBot導入

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
   factory :item do
-    
+    item_name           {Faker::Lorem.sentence}
+    text                {Faker::Lorem.sentence}
+    image               {Faker::Lorem.sentence}
+    price               { 1000 }
+    category_id         { 1 }
+    status_id           { 1 }
+    shipping_fee_id     { 1 }
+    prefecture_id       { 1 }
+    date_of_shipment_id { 1 }
+    association :user 
   end
 end
