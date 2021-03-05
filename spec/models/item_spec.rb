50518b787a1ebb2b299c14b6fed8fb1c21f38112
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-05 12:12:12 +0900

    商品が出品できる場合のテストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe Item, type: :model do
   before do
     @item = FactoryBot.build(:item)
   end
+
+  describe '商品の出品' do
+    context '商品を出品できる場合' do
+      it '全ての項目が正しく入力されていれば出品できる' do
+        expect(@item).to be_valid
+      end 
+    end
+    context '商品を出品できない場合' do
+      
+    end
+  end
 end
