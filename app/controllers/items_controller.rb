42039d13e9b0904a404bbf2d9ee45c5fb0954a7a
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-07 17:52:47 +0900

    createアクションの訂正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,8 +8,8 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
-    if @item.created_at
+    @item = Item.new(item_params)
+    if @item.save
       redirect_to root_path
     else
       render :new
