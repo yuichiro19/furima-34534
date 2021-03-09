8b6c2306a1cc8d7785057a8b818914da5d617c5a
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-09 11:50:08 +0900

    ログアウト状態のユーザーの編集ページへの遷移制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   def index
     @items = Item.all.order('created_at DESC')
   end
