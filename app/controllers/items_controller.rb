743fe3180e42963c84576c0792a59fd866584059
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-09 14:24:29 +0900

    重複している条件の削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,6 +46,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless user_signed_in? && current_user.id == @item.user_id
+    redirect_to action: :index unless current_user.id == @item.user_id
   end
 end
