343dfb70fcbb37541d5f794b0b381a74a2943029
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-09 12:01:32 +0900

    他者の商品情報編集ページへのアクセス制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :move_to_index, only: :edit
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -39,5 +41,11 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :image, :text, :price, :category_id, :status_id, :shipping_fee_id, :prefecture_id,
                                  :date_of_shipment_id).merge(user_id: current_user.id)
+  end
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
   end
 end
