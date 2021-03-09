d28bc5238f0cb912e1c01bc37b191015a8872261
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-09 14:34:52 +0900

    ルートパスの簡略化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -28,7 +28,7 @@ class ItemsController < ApplicationController
 
   def update
     if @item.update(item_params)
-      redirect_to item_path, method: :get
+      redirect_to item_path
     else
       render :edit
     end
