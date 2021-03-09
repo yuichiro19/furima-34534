978d23c0dd84178ecacc560f22ede764b4b4facb
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-09 15:11:39 +0900

    ログインしてないと削除できない制限の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, only: [:edit, :update]
 
