b972139c5604c0c959f004c388b5335d842fb55b
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-10 15:03:28 +0900

    shipping_addressモデルのバリデーション設定(仮)

--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -2,4 +2,12 @@ class ShippingAddress < ApplicationRecord
     extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :purchase
     belongs_to :prefecture
+
+    with_options presence: true do
+        validates :postal_code
+        validates :prefecture_id
+        validates :city
+        validates :address
+        validates :phone_number
+    end
 end
