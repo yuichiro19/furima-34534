e1de45349e6de4079be9b653429be4c6d8cb5d04
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-10 15:16:36 +0900

    Formオブジェクトの定義(仮)

--- app/models/formobject.rb
+++ app/models/formobject.rb
@@ -1,3 +1,18 @@
 class FormObject 
     include ActiveModel::Model
+    attr_accessor :user, :item, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase
+    
+    with_options presence: true do
+        validates :postal_code
+        validates :prefecture_id
+        validates :city
+        validates :address
+        validates :phone_number
+    end
+
+    def save
+        Purchase.create(:user_id, :item_id)
+        ShippingAddress.create(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase)
+    end
+
 end
