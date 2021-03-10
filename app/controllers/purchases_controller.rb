8e14fbf23254f9f43126e99abaae1bf4626a572d
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-10 16:26:49 +0900

    index,createアクションの定義、ストロングパラメーターの設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,14 @@
 class PurchasesController < ApplicationController
     def index
+        @form_object = FormObject.new
     end
 
     def create
+        @form_object = FormObject.new(form_object_params)
+    end
+
+    private
+    def form_object_params
+        params.require(:form_object).permit(:item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase_id).merge(user_id: current_user.id)
     end
 end
