fd05158a226e81557f68e4b016ff4ed55f677184
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-11 12:54:45 +0900

    ファイル名の訂正

--- app/models/form_object.rb
+++ app/models/form_object.rb
@@ -11,8 +11,8 @@ class FormObject
     end
 
     def save
-        Purchase.create(:user_id, :item_id)
-        ShippingAddress.create(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase_id)
+        purchase = Purchase.create(user_id: user_id, item_id: item_id)
+        ShippingAddress.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, purchase_id: purchase.id)
     end
 
 end
