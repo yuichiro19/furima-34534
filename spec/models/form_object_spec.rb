02cf9e64491d42717d096c67c4e11fc42c765b7b
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-11 18:01:03 +0900

    商品が購入できる際のexample追加、商品が購入できない際のexample列挙

--- spec/models/form_object_spec.rb
+++ spec/models/form_object_spec.rb
@@ -10,6 +10,63 @@ RSpec.describe FormObject, type: :model do
             it '全ての項目が正しく入力されていれば購入できる' do
                 expect(@form_object).to be_valid
             end
+            it 'buildingが空でも他の項目が正しく入力されていれば購入できる' do
+                @form_object.building = ''
+                expect(@form_object).to be_valid
+            end
+        end
+        context '商品が購入できない場合' do
+            it 'postal_codeが空では購入できない' do
+                
+            end
+            it 'postal_codeが全角だと購入できない' do
+                
+            end
+            it 'postal_codeが半角英数混合だと購入できない' do
+                
+            end
+            it 'postal_codeが半角英語だと購入できない' do
+                
+            end
+            it 'postal_codeに-が含まれていないと購入できない' do
+                
+            end
+            it 'postal_codeに-が含まれてても-の位置が違うと購入できない' do
+                
+            end
+            it 'prefecture_idが空では購入できない'do
+                
+            end
+            it 'prefecture_idが未選択では購入できない' do
+                
+            end
+            it 'cityが空では購入できない' do
+                
+            end
+            it 'addressが空では購入できない' do
+                
+            end
+            it 'phone_numberが空では購入できない' do
+                
+            end
+            it 'phone_numberが12桁以上では購入できない' do
+                
+            end
+            it 'phone_numberが全角だと購入できない' do
+                
+            end
+            it 'phone_numberが半角英数混合だと購入できない' do
+                
+            end
+            it 'phone_numberが半角英語だと購入できない' do
+                
+            end
+            it 'userが紐づいていなければ購入できない' do
+                
+            end
+            it 'itemが紐づいていなければ購入できない' do
+                
+            end
         end
     end
 end
