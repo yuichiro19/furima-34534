73291e14aa61221f196b3eb78d321bd0aa01daad
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-11 17:37:32 +0900

    商品が購入できる際のテストコード記述

--- spec/models/form_object_spec.rb
+++ spec/models/form_object_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe FormObject, type: :model do
+    before do
+        @form_object = FactoryBot.build(:form_object)
+    end
+
+    describe '商品の購入' do
+        context '商品の購入ができる場合' do
+            it '全ての項目が正しく入力されていれば購入できる' do
+                expect(@form_object).to be_valid
+            end
+        end
+    end
 end
