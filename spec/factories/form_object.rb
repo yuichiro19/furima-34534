90a60d7a87531daddc911ba67ba598fcb5adf16b
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-11 17:37:06 +0900

    FormObjectモデルテストコード用のFactoryBot修正

--- spec/factories/form_object.rb
+++ spec/factories/form_object.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
     factory :form_object do
-      postal_code   { Faker::Number.number(3) + '-' + Faker::Number.number(4) }
+      postal_code   { '000-0000' }
       prefecture_id { 1 }
       city          { 'test' }
       address       { 'test' }
       building      { 'test' }
-      phone_number  { Faker::Number.number(max_length: 11) }
-      association :user, :item 
+      phone_number  { '09000000000' }
+      user_id       { 1 }
+      item_id       { 1 }
 
     end
   end
