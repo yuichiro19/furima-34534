108121edaa30d7828944a6d30bc8189010ea6083
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-11 14:21:10 +0900

    Formオブジェクトのバリデーション再設定

--- app/models/form_object.rb
+++ app/models/form_object.rb
@@ -3,12 +3,14 @@ class FormObject
     attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase_id
     
     with_options presence: true do
-        validates :postal_code
-        validates :prefecture_id
+        validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
         validates :city
         validates :address
-        validates :phone_number
+        validates :phone_number, format: {with: /\A[0-9]{1, 11}\z/, message: "number Input only number"}
+        validates :user_id
+        validates :item_id
     end
+    validates :prefecture_id, numericality: { other_than: 0, message: 'Select'}
 
     def save
         purchase = Purchase.create(user_id: user_id, item_id: item_id)
--- app/models/shipping_address.rb
+++ app/models/shipping_address.rb
@@ -2,12 +2,5 @@ class ShippingAddress < ApplicationRecord
     extend ActiveHash::Associations::ActiveRecordExtensions
     belongs_to :purchase
     belongs_to :prefecture
-
-    with_options presence: true do
-        validates :postal_code
-        validates :prefecture_id
-        validates :city
-        validates :address
-        validates :phone_number
-    end
+    
 end
