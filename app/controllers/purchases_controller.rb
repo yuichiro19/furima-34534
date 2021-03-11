eb0d536374871a6f60daa0593d3c5fbbac49bb50
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-11 12:56:37 +0900

    購入履歴と配送先住所の保存処理

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,14 +1,22 @@
 class PurchasesController < ApplicationController
     def index
         @form_object = FormObject.new
+        @item = Item.find(params[:item_id])
     end
 
     def create
         @form_object = FormObject.new(form_object_params)
+        @item = Item.find(params[:item_id])
+        if @form_object.valid?
+            @form_object.save
+            redirect_to root_path
+        else 
+            render :index
+        end 
     end
 
     private
     def form_object_params
-        params.require(:form_object).permit(:item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase_id).merge(user_id: current_user.id)
+        params.require(:form_object).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase_id).merge(user_id: current_user.id, item_id: params[:item_id])
     end
 end
