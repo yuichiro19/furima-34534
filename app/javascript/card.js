aefba992d18a30384b138526102a8ae5bba16bea
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-12 16:14:57 +0900

    カードの入力情報をJSファイルに渡す記述

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,19 @@
 const pay = () => {
-    console.log("カード情報トークン化のためのJavaScript");
-  };
+    Payjp.setPublicKey(pk_test_5c27a31a39d02f5d6e66c59e)
+    const form = document.getElementById("charge-form");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        
+        const formResult = document.getElementById("charge-form");
+        const formData = new FormData(formResult);
+    
+        const card = {
+          number: formData.get("order[number]"),
+          cvc: formData.get("order[cvc]"),
+          exp_month: formData.get("order[exp_month]"),
+          exp_year: `20${formData.get("order[exp_year]")}`,
+        };
+    });
+};
   
   window.addEventListener("load", pay);
