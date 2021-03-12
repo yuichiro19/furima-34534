50a6f4f3e79b3a9d356f1cf9b53360b2463032c0
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-12 18:57:13 +0900

    tokenの情報を扱えるようにFormオブジェクトの記述変更

--- app/models/form_object.rb
+++ app/models/form_object.rb
@@ -1,6 +1,6 @@
 class FormObject 
     include ActiveModel::Model
-    attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase_id
+    attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :purchase_id, :token
     
     with_options presence: true do
         validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "Input correctly"}
