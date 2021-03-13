d57ef2bd9bc589a30fd7817d41971c5c531e8102
yuichiro19 <78407847+yuichiro19@users.noreply.github.com>
2021-03-13 20:10:51 +0900

    tokenのバリデーション設定

--- app/models/form_object.rb
+++ app/models/form_object.rb
@@ -9,6 +9,7 @@ class FormObject
         validates :phone_number, format: {with: /\A[0-9]{,11}\z/, message: "Input only number"}
         validates :user_id
         validates :item_id
+        validates :token
     end
     validates :prefecture_id, numericality: { other_than: 0, message: 'Select'}
 
